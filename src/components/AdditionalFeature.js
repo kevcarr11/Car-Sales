fe1ffe52d68abef16b384358f75dec92fd50b06c
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-22 17:04:46 -0500

    built action creator for adding a feature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 const AdditionalFeature = props => {
+
+    const handleAdd = () => {
+
+    }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={handleAdd} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapDispatchToProps = {
+  
+}
+
+export default connect()(AdditionalFeature);
